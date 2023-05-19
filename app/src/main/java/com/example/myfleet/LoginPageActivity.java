beff6e0e52641a7c2c36e8ee02ad493b66f01e57
LeonardoMonteiro02 <lmdsp12@gmailleonardo.pinto1@estudante.ufla.br>
2023-05-19 17:27:16 -0300

    Implementação do login com facebook e Gmail

--- app/src/main/java/com/example/myfleet/LoginPageActivity.java
+++ app/src/main/java/com/example/myfleet/LoginPageActivity.java
@@ -1,14 +1,32 @@
 package com.example.myfleet;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.Arrays;
+
 
 public class LoginPageActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +34,9 @@ public class LoginPageActivity extends AppCompatActivity implements View.OnClick
     private Button loginFacebookButton;
     private Button loginButton;
     private TextView createAccountTextView;
+    private static final int RC_SIGN_IN = 9001; // Código de solicitação de login
+    private GoogleApiClient mGoogleApiClient;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +51,24 @@ public class LoginPageActivity extends AppCompatActivity implements View.OnClick
         Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/LTC Kennerley W00 Small Caps.ttf");
         createAccountTextView.setTypeface(customFont);
 
+        // Configurar opções de login pelo Gmail
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Configurar o CallbackManager para autenticação pelo Facebook
+        callbackManager = CallbackManager.Factory.create();
+
+// Criar o cliente da API do Google
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        // Tratar falhas na conexão
+                        Toast.makeText(LoginPageActivity.this, "Falha na conexão com o Google Play Services", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         loginEmailButton.setOnClickListener(this);
         loginFacebookButton.setOnClickListener(this);
@@ -40,17 +79,91 @@ public class LoginPageActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.buttonLoginEmail) {
-            Toast.makeText(this, "Entrar com Gmail pressionado", Toast.LENGTH_SHORT).show();
-            // Adicione aqui a lógica para a ação do botão "Entrar com Gmail"
+            // Iniciar a solicitação de login pelo Gmail
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
         } else if (v.getId() == R.id.buttonFacebook) {
-            Toast.makeText(this, "Entrar com Facebook pressionado", Toast.LENGTH_SHORT).show();
-            // Adicione aqui a lógica para a ação do botão "Entrar com Facebook"
+            // Solicitar permissões adicionais, se necessário
+            LoginManager.getInstance().logInWithReadPermissions(LoginPageActivity.this, Arrays.asList("email"));
+
+// Registrar o callback para tratar o resultado da autenticação
+            LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    // Autenticação pelo Facebook bem-sucedida
+                    // Obter informações do usuário a partir do loginResult
+                    // Exemplo: obter o token de acesso
+                    AccessToken accessToken = loginResult.getAccessToken();
+                    String token = accessToken.getToken();
+
+                    // Exemplo: exibir o token em um Toast
+                    Toast.makeText(LoginPageActivity.this, "Token de acesso: " + token, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancel() {
+                    // Autenticação pelo Facebook cancelada pelo usuário
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                    // Ocorreu um erro durante a autenticação pelo Facebook
+                }
+            });
         } else if (v.getId() == R.id.buttonLogin) {
             Toast.makeText(this, "Login Criado pressionado", Toast.LENGTH_SHORT).show();
             // Adicione aqui a lógica para a ação do botão "Login Criado"
         } else if (v.getId() == R.id.textViewCreateAccount) {
             Toast.makeText(this, "Problemas com login? pressionado", Toast.LENGTH_SHORT).show();
             // Adicione aqui a lógica para a ação do texto "Problemas com login?"
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Verificar o código de solicitação para determinar a origem do resultado
+        if (requestCode == RC_SIGN_IN) {
+            // Resultado da autenticação do Google
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleGoogleSignInResult(googleSignInResult);
+        } else {
+            // Resultado da autenticação do Facebook
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void handleGoogleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Login bem-sucedido, obter a conta do usuário
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            // Aqui você pode realizar as ações necessárias com a conta do usuário
+            String fullName = account.getDisplayName();
+            String email = account.getEmail();
+
+            // Exemplo: exibir o nome e o email em um Toast
+            Toast.makeText(this, "Nome: " + fullName + "\nEmail: " + email, Toast.LENGTH_SHORT).show();
+        } else {
+            // Falha no login, exibir uma mensagem de erro
+            Toast.makeText(this, "Falha no login pelo Gmail", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Login bem-sucedido, obter a conta do usuário
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            // Aqui você pode realizar as ações necessárias com a conta do usuário (por exemplo, enviar para o servidor, iniciar a próxima atividade, etc.)
+            String fullName = account.getDisplayName();
+            String email = account.getEmail();
+
+            // Exemplo: exibir o nome e o email em um Toast
+            Toast.makeText(this, "Nome: " + fullName + "\nEmail: " + email, Toast.LENGTH_SHORT).show();
+        } else {
+            // Falha no login, exibir uma mensagem de erro
+            Toast.makeText(this, "Falha no login pelo Gmail", Toast.LENGTH_SHORT).show();
         }
     }
 }
